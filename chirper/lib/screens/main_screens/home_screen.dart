65334cb22d154bc1734c79584646b0079993f0d0
Ashish Shetty <30448930+Shetty073@users.noreply.github.com>
2021-09-10 19:48:02 +0530

    fixed #1   newly added chirps are displayed in the correct order i.e. latest first

--- chirper/lib/screens/main_screens/home_screen.dart
+++ chirper/lib/screens/main_screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:chirper/components/chirp_card/chirp_card.dart';
 import 'package:chirper/data/models/chirp.dart';
 import 'package:chirper/services/boxes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hive/hive.dart';
@@ -20,30 +21,35 @@ class _HomeScreenState extends State<HomeScreen> {
       child: ValueListenableBuilder<LazyBox<Chirp>>(
         valueListenable: Boxes.getChirps().listenable(),
         builder: (context, box, widget) {
-          return ListView.builder(
-            itemCount: box.length,
-            itemBuilder: (context, index) {
-              return FutureBuilder(
-                future: box.getAt(index),
-                builder: (context, snapshot) {
-                  if(snapshot.connectionState == ConnectionState.done) {
-                    final Chirp _chirp = snapshot.data as Chirp;
-                    return ChirpCard(
-                      chirp: _chirp,
-                    );
-                  } else {
-                    return Container(
-                      child: Center(
-                        child: SpinKitCircle(
-                          size: 10.0,
-                          color: Theme.of(context).bottomNavigationBarTheme.unselectedItemColor,
+          return Align(
+            alignment: Alignment.topCenter,
+            child: ListView.builder(
+              reverse: true,
+              shrinkWrap: true,
+              itemCount: box.length,
+              itemBuilder: (context, index) {
+                return FutureBuilder(
+                  future: box.getAt(index),
+                  builder: (context, snapshot) {
+                    if(snapshot.connectionState == ConnectionState.done) {
+                      final Chirp _chirp = snapshot.data as Chirp;
+                      return ChirpCard(
+                        chirp: _chirp,
+                      );
+                    } else {
+                      return Container(
+                        child: Center(
+                          child: SpinKitCircle(
+                            size: 10.0,
+                            color: Theme.of(context).bottomNavigationBarTheme.unselectedItemColor,
+                          ),
                         ),
-                      ),
-                    );
-                  }
-                },
-              );
-            },
+                      );
+                    }
+                  },
+                );
+              },
+            ),
           );
         },
       ),
