ab9f6dc8d6e4a764665f93553aabc33a6f3a8de4
Ashish Shetty <30448930+Shetty073@users.noreply.github.com>
2021-09-29 23:01:28 +0530

    add draft-js editor

--- web-frontend/src/components/chirpeditor.component.js
+++ web-frontend/src/components/chirpeditor.component.js
@@ -1,18 +1,31 @@
-import {Button, Col, Form} from "react-bootstrap";
+import React from 'react';
+import {Editor, EditorState} from "draft-js";
 
 const ChirpEditorComponent = () => {
+	const [editorState, setEditorState] = React.useState(() =>
+		EditorState.createEmpty()
+	);
+
+	const editor = React.useRef(null);
+	const focusEditor = () => {
+		editor.current.focus();
+	}
+
 	return (
 		<div className="my-3 w-100">
-			<Col>
-				<Form>
-					<Form.Group className="mb-3" controlId="chirp">
-						<Form.Control as="textarea" placeholder="What's on your mind?" required/>
-					</Form.Group>
-					<Button variant="primary" type="submit">
-						Chirp
-					</Button>
-				</Form>
-			</Col>
+			<div
+				style={{ border: "1px solid lightgrey", minHeight: "6em", cursor: "text" }}
+				className="p-1"
+				onClick={focusEditor}
+			>
+			<Editor
+				ref={editor}
+				editorState={editorState}
+				onChange={setEditorState}
+				placeholder="What's on your mind?"
+			/>
+			</div>
+
 		</div>
 	);
 }
