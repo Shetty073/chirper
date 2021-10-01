66c856df660174178f704b4eafd754584a5f33ff
Ashish Shetty <30448930+Shetty073@users.noreply.github.com>
2021-10-02 02:51:30 +0530

    replace daftjs with react-mentions

--- web-frontend/src/components/chirpeditor.component.js
+++ web-frontend/src/components/chirpeditor.component.js
@@ -1,33 +1,127 @@
-import React from 'react';
-import {Editor, EditorState} from "draft-js";
+import React, {useState} from 'react';
+import { MentionsInput, Mention } from 'react-mentions';
+import {Button, Col} from "react-bootstrap";
 
 const ChirpEditorComponent = () => {
-	const [editorState, setEditorState] = React.useState(() =>
-		EditorState.createEmpty()
-	);
+	const [chirp, setChirp] = useState('');
+	const [chirpPlainText, setChirpPlainText] = useState('');
+	const [mentionActive, setMentionActive] = useState(false);
+	const [loading, setLoading] = useState(false);
 
-	const editor = React.useRef(null);
-	const focusEditor = () => {
-		editor.current.focus();
+
+	const submit = () => {
+		setLoading(true);
+
+		// TODO: Submit data code goes here
+
+		setLoading(false);
 	}
 
-	return (
-		<div className="my-3 w-100">
-			<div
-				style={{ border: "1px solid lightgrey", minHeight: "6em", cursor: "text" }}
-				className="p-1"
-				onClick={focusEditor}
-			>
-			<Editor
-				ref={editor}
-				editorState={editorState}
-				onChange={setEditorState}
-				placeholder="What's on your mind?"
-			/>
-			</div>
+	const handleChange = (event, newValue, newPlainTextValue, mentions) => {
+		setChirp(newValue);
+		setChirpPlainText(newPlainTextValue);
+	}
 
-		</div>
+	const fetchUsernameSuggestions = (query, callback) => {
+		// TODO: Replace this demo code with actual backend API call for username list
+		if (!query) return
+		fetch(`https://api.github.com/search/users?q=${query}`, { json: true })
+			.then(res => res.json())
+			// Transform the users to what react-mentions expects
+			.then(res =>
+				res.items.map(user => ({ display: user.login, id: user.login }))
+			)
+			.then(callback);
+	}
+
+	const fetchHashTagSuggestions = (query, callback) => {
+		// TODO: Replace this demo code with actual backend API call for trending hashtags list
+		if (!query) return
+		fetch(`https://api.github.com/search/users?q=${query}`, { json: true })
+			.then(res => res.json())
+			// Transform the users to what react-mentions expects
+			.then(res =>
+				res.items.map(user => ({ display: user.login, id: user.login }))
+			)
+			.then(callback);
+	}
+
+	const renderUsernameSuggestion = (suggestion, search, highlightedDisplay) => (
+		<div className="user">{highlightedDisplay}</div>
+	);
+
+	const renderTagSuggestion = (suggestion, search, highlightedDisplay) => (
+		<div className="tag">{highlightedDisplay}</div>
+	);
+
+	return (
+			<div className="my-3 w-100" id="editor-js">
+				<Col>
+					<MentionsInput
+						value={chirp}
+						onChange={handleChange}
+						placeholder={"What's on your mind?"}
+						style={editorStyle}
+						>
+						<Mention
+							trigger="@"
+							data={fetchUsernameSuggestions}
+							renderSuggestion={renderUsernameSuggestion}
+							style={mentionStyle}
+							appendSpaceOnAdd={true}
+						/>
+						<Mention
+							trigger="#"
+							data={fetchHashTagSuggestions}
+							renderSuggestion={renderTagSuggestion}
+							style={hashTagStyle}
+							appendSpaceOnAdd={true}
+						/>
+					</MentionsInput>
+					<Button variant="primary" className="mt-2" onClick={submit}>
+						Chirp
+					</Button>
+				</Col>
+			</div>
 	);
 }
+
+const editorStyle = {
+	input: {
+		overflow: 'auto',
+		height: 70,
+		textRendering: 'geometricPrecision',
+	},
+
+	highlighter: {
+		boxSizing: 'border-box',
+		overflow: 'hidden',
+		height: 70,
+	},
+};
+
+const mentionStyle = {
+	paddingLeft: 2,
+	paddingBottom: 2,
+	backgroundColor: '#fff',
+	position: 'relative',
+	zIndex: 1,
+	color: '#0b5ed7',
+	textShadow: '1px 1px 1px white, 1px -1px 1px white, -1px 1px 1px white,\n' +
+		'\t-1px -1px 1px white',
+	pointerEvents: 'none',
+};
+
+const hashTagStyle = {
+	paddingLeft: 2,
+	paddingBottom: 2,
+	backgroundColor: '#fff',
+	position: 'relative',
+	zIndex: 1,
+	color: '#85a4ea',
+	textShadow: '1px 1px 1px white, 1px -1px 1px white, -1px 1px 1px white,\n' +
+		'\t-1px -1px 1px white',
+	pointerEvents: 'none',
+};
 
 export default ChirpEditorComponent;
